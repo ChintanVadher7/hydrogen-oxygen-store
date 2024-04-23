4201e066f9798f9d37bbde0be3c128a3fe6bb0d1
Chintan <132256238+ChintanVadher7@users.noreply.github.com>
2024-04-23 12:24:03 +0530

    config file changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
+import { installGlobals } from "@remix-run/node";
 import { hydrogen } from '@shopify/hydrogen/vite';
 import { oxygen } from '@shopify/mini-oxygen/vite';
 import { vitePlugin as remix } from '@remix-run/dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+installGlobals();
 
 export default defineConfig({
   plugins: [
@@ -10,10 +13,22 @@ export default defineConfig({
     oxygen(),
     remix({
       presets: [hydrogen.preset()],
-      ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.scss", "**/*.css.map"],
+      appDirectory: 'app',
+      ignoredRouteFiles: ['**/.*'],
+      watchPaths: ['./public', './.env'],
+      server: './server.js',
+      publicPath: (process.env.HYDROGEN_ASSET_BASE_URL ?? '/') + 'build/',
+      assetsBuildDirectory: 'dist/client/assets',
+      serverBuildPath: 'dist/server/index.js',
+      serverMainFields: ['browser', 'module', 'main'],
+      serverConditions: ['worker', process.env.NODE_ENV],
+      serverDependenciesToBundle: 'all',
+      serverModuleFormat: 'esm',
+      serverPlatform: 'neutral',
+      serverMinify: process.env.NODE_ENV === 'production',
       future: {
         v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
+        v3_relativeSplatpath: true,
         v3_throwAbortReason: true,
       },
     }),
